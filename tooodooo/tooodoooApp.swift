41e3411cb7c4bf7de14b7e63b074f9ab4bc0dcfc
Pop Plays <ng.qi.an@sji.edu.sg>
2023-07-29 11:08:13 +0800

    Initial Commit

---
+++ tooodooo/tooodoooApp.swift
@@ -0,0 +1,17 @@
+//
+//  tooodoooApp.swift
+//  tooodooo
+//
+//  Created by Ng Qi An on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct tooodoooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
